eabcb2006cf043eaec02d9052a9cb910a8e09bda
ianmagdaleno <54992934+ianmagdaleno@users.noreply.github.com>
2020-03-16 00:11:27 -0300

    Maquina de estado e inicio dos comandos aparecendo
    programacao para identificacao dos triggers da maquina de estado com lista infinita e relaciona entre trigger e objetivo

---
+++ QuintoSemestre/Assets/Characters/ControleObjetivo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControleObjetivo : MonoBehaviour
+{
+    [SerializeField] GameObject[] l_trigger;
+    [SerializeField] Sprite[] l_objetivo;
+
+    public int indice;
+    void Start()
+    {
+        indice = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        for ( int x = indice; indice < l_trigger.Length; indice++)
+        {
+            Debug.Log(indice);
+            Debug.Log("bateu");
+
+            l_trigger[indice].SetActive(false);
+
+
+            indice++;
+            break;
+        }
+    }
+}
