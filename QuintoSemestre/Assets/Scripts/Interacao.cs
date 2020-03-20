cd6675c3fc02eb746041d8f34c4a1225c861487b
AmandaChristina <amanda.christina.n@gmail.com>
2020-03-20 16:51:31 -0300

    Tentando implementar selecionavel

---
+++ QuintoSemestre/Assets/Scripts/Interacao.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interacao : MonoBehaviour
+{
+    public float distance = 10;
+    public bool press_button = false;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //laser cast AMANDA 
+        Ray raio = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(raio.origin, raio.direction * distance);
+        RaycastHit hit;
+        Selecionavel selecao = null;
+
+
+        if (Physics.Raycast(raio, out hit, distance)){
+            selecao = hit.transform.GetComponent<Selecionavel>();
+        }
+        if(selecao != null){
+            selecao.Selecionar();
+        }
+
+    }
+}
---
+++ QuintoSemestre/Assets/Scripts/Selecionavel.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selecionavel : MonoBehaviour
+{
+  
+
+    MeshRenderer renderer;
+    bool selecionado;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void LateUpdate()
+    {
+        if (selecionado)
+        {
+            renderer.material.SetColor("_EmissionColor", Color.grey);
+            renderer.material.EnableKeyword("_EMISSION");
+
+            
+        }
+
+        else
+        {
+            renderer.material.DisableKeyword("_EMISSION");
+           
+        }
+
+        selecionado = false;
+    }
+
+    public void Selecionar()
+    {
+        selecionado = true;
+    }
+
+    
+}
