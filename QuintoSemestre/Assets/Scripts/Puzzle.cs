31e66630c72915f9d16c0761b68105062e4dc79a
Victor <61842979+Dirtcake@users.noreply.github.com>
2020-03-23 01:45:17 -0300

    Alteração no input da interação
    Melhor funionamento dos inputs no puzzle

--- QuintoSemestre/Assets/Scripts/Puzzle.cs
+++ QuintoSemestre/Assets/Scripts/Puzzle.cs
@@ -13,6 +13,7 @@ public class Puzzle : MonoBehaviour {
     bool camera_open, trigger_check;
 
     public GameObject cameraPlayer, panorama;
+    private bool entrou, saiu;
 
     void Start () {
 
@@ -20,21 +21,28 @@ public class Puzzle : MonoBehaviour {
 
     void Update () {
 
-       // Open_camera ();
+        // Open_camera ();
         movimentacao_nacamera ();
+
+        if (Input.GetKeyDown (KeyCode.E)) entrou = true;
+        if (Input.GetKeyDown (KeyCode.Q)) saiu = true;
 
     }
 
     void OnTriggerStay (Collider other) {
 
-        if (other.tag == "Player" && camera_open == false && Input.GetKeyDown (KeyCode.E)) {
+        if (other.tag == "Player" && camera_open == false && entrou) {
 
-            abrir_camera();
+            abrir_camera ();
             camera_open = true;
+
+            entrou = false;
         }
-        if (other.tag == "Player" && camera_open == true && Input.GetKeyDown (KeyCode.Q)) {
-            fechar_camera();
+        if (other.tag == "Player" && camera_open == true && saiu) {
+            fechar_camera ();
             camera_open = false;
+
+            saiu = false;
         }
     }
 
