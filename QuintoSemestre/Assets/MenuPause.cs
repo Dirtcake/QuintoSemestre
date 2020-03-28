c6836dea86008df9c4125ca5b4fe1a16809bd25e
Piraldo <guilherme.msb99@gmail.com>
2020-03-28 19:09:07 -0300

    PauseBlocado
    Menu pause para o jogo, menos movimentos do mouse, botões ainda não funcionais

---
+++ QuintoSemestre/Assets/MenuPause.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPause : MonoBehaviour
+{
+    public static bool JogoPausado = false;
+    public GameObject MenuPauseUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (JogoPausado)
+            {
+                Resume();   
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Resume()
+    {
+        MenuPauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        JogoPausado = false;
+    }
+    void Pause()
+    {
+        MenuPauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        JogoPausado = true;
+    }
+}
+
