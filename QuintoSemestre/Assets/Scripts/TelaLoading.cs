0c659d9dc5d4c4c9fc736f540420b20c4f277f61
Piraldo <guilherme.msb99@gmail.com>
2020-04-08 00:26:49 -0300

    Tela de loading carregando
    carrega muito rápido (?)

---
+++ QuintoSemestre/Assets/Scripts/TelaLoading.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TelaLoading : MonoBehaviour
+{
+    [SerializeField]
+    private Image Progresso;
+ 
+    void Start()
+    {
+        StartCoroutine(LoadAsyncOperation());
+    }
+
+    IEnumerator LoadAsyncOperation()
+    {
+        AsyncOperation gameLevel = SceneManager.LoadSceneAsync(2);
+
+        while (gameLevel.progress < 1)
+        {
+            Progresso.fillAmount = gameLevel.progress;
+            yield return new WaitForEndOfFrame();
+        }
+        
+    }
+
+}
