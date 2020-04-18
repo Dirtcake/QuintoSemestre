ca39272bebdffe50a500e84df5acd7ec0131dfd0
ianmagdaleno <54992934+ianmagdaleno@users.noreply.github.com>
2020-04-18 18:06:13 -0300

    limpa no codigo de objetivo

--- QuintoSemestre/Assets/Characters/ControleObjetivo.cs
+++ QuintoSemestre/Assets/Characters/ControleObjetivo.cs
@@ -7,23 +7,26 @@ public class ControleObjetivo : MonoBehaviour
 {
     [SerializeField] GameObject[] l_trigger;
     [SerializeField] Sprite[] l_objetivo;
+    [SerializeField] GameObject[] l_triggerV;
     [SerializeField] VideoPlayer[] l_videos;
 
-    public RawImage fundoVideo;
-    public AudioClip somLugar;
+    int indice;
+    int indiceV;
 
+    public RawImage fundoVideo;
+    float alpha;
+
+    public AudioClip somLugar;
     public AudioClip som;
-    public int indice;
-    AudioSource sons;    
+    AudioSource sons; 
+    
     public GameObject objetivos;
 
-    public float alpha ;
+    
 
     void Start()
     {
-        indice = 0;
         sons = GetComponent<AudioSource>();
-     //   fundoVideo = GetComponent<RawImage>();
 
         fundoVideo.CrossFadeAlpha(alpha, 0f, true);
     }
@@ -31,35 +34,28 @@ public class ControleObjetivo : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        /* for ( int x = indice; indice < l_trigger.Length; indice++)
-         {
-          //   Debug.Log(indice);
-          //  Debug.Log("bateu");
-              l_trigger[indice].SetActive(false);
-              objetivos.GetComponent<Image>().sprite = l_objetivo[indice];
-              sons.clip = som;
-             sons.Play();
-
-             indice++;
-             break;
-         }*/
+       
         if (other.CompareTag("estado") && indice < l_trigger.Length)
         {
-            
-            Debug.Log("bateu");
-
-
             objetivos.GetComponent<Image>().sprite = l_objetivo[indice];
-            fundoVideo.CrossFadeAlpha(1f, 0f, true);
-            fundoVideo.texture = l_videos[indice].texture;
-            l_videos[indice].Play();
-           
+          
             sons.clip = som;
             sons.Play();
 
             l_trigger[indice].SetActive(false);
             other.gameObject.SetActive(false);
             indice++;
+        }
+        else if (other.CompareTag("lugar") && indiceV < l_triggerV.Length)
+        {
+
+            fundoVideo.CrossFadeAlpha(1f, 0f, true);
+            fundoVideo.texture = l_videos[indiceV].texture;
+            l_videos[indiceV].Play();
+
+            l_triggerV[indiceV].SetActive(false);
+            other.gameObject.SetActive(false);
+            indiceV++;
         }
     }
 }
