2c92792e1ba14e3913beaa60c12e61907150b4b6
Victor <61842979+Dirtcake@users.noreply.github.com>
2020-05-03 19:29:03 -0300

    Hub J2

---
+++ QuintoSemestre/Assets/Arido/Scripts/TriggerScripts/HubEvents.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class HubEvents : MonoBehaviour
+{
+    public List<VideoClip> VideoClips;
+
+
+  public void NomearLocal()
+    {
+
+        GetComponent<VideoPlayer>().clip = VideoClips[0];
+        GetComponent<VideoPlayer>().Play();
+    }
+}
---
+++ QuintoSemestre/Assets/Arido/Scripts/TriggerScripts/TriggerEvent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEvent : MonoBehaviour
+{
+    public UnityEvent Triggered;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        Triggered.Invoke();
+    }
+}
