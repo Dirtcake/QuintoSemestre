481206ab67213dea119a2d2931ac600660719b58
ianmagdaleno <54992934+ianmagdaleno@users.noreply.github.com>
2020-05-04 03:31:47 -0300

    puzzle1 inicio

---
+++ QuintoSemestre/Assets/Arido/Scripts/Puzzle1-1/ChaoFalso.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaoFalso : MonoBehaviour
+{
+    public Vector3 caiu;
+    public Vector3 normal;
+    void Start()
+    {
+        normal = transform.position;
+    }
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collider col )
+    {
+       
+    }
+}
