38934ba6d2740fac87197def4490b4e20e2d2d4c
Victor <61842979+Dirtcake@users.noreply.github.com>
2020-05-04 19:12:39 -0300

    Junção

---
+++ QuintoSemestre/Assets/Arido/Scripts/TriggerScripts/TutoEvents.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutoEvents : MonoBehaviour
+{
+public void trocaCena()
+    {
+        SceneManager.LoadScene("Hub");
+    }
+}
