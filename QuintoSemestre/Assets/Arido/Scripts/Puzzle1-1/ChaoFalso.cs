2ac98fa6b0c62f15ea12fd3e4305a03f14959237
ianmagdaleno <54992934+ianmagdaleno@users.noreply.github.com>
2020-05-04 05:32:37 -0300

    tiles posicionados puzzle1

--- QuintoSemestre/Assets/Arido/Scripts/Puzzle1-1/ChaoFalso.cs
+++ QuintoSemestre/Assets/Arido/Scripts/Puzzle1-1/ChaoFalso.cs
@@ -5,22 +5,30 @@ using UnityEngine;
 public class ChaoFalso : MonoBehaviour
 {
     public Vector3 caiu;
+   
     Vector3 posicao;
+    Player jogador;
+   
     void Start()
     {
         posicao = transform.localPosition;
+        jogador = GetComponent<Player>();
+        
     }
     void Update()
     {
 
     }
-    private void OnCollisionEnter(Collider col)
+    private void OnCollisionEnter(Collision col)
     {
-        if (CompareTag("Player"))
+        if (col.gameObject.CompareTag("Player"))
         {
             
             posicao = Vector3.Lerp(posicao, caiu, 2 * Time.deltaTime);
+            Debug.Log("bateu certo");
             
         }
+        Debug.Log("bateu na" + col.collider.tag);
     }
+    
 }
