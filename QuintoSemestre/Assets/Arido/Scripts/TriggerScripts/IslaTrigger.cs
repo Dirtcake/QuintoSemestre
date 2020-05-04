252cb9ae6952c46a38e557b8a600beb4df7b6b2c
Victor <61842979+Dirtcake@users.noreply.github.com>
2020-05-04 20:03:17 -0300

    Last Breath

---
+++ QuintoSemestre/Assets/Arido/Scripts/TriggerScripts/IslaTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IslaTrigger : MonoBehaviour
+{
+    public void trocaCena()
+    {
+        SceneManager.LoadScene("FinalJ2");
+    }
+}
