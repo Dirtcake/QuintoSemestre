61a0610488cc2e1c145348a0c95620a29210f769
ianmagdaleno <54992934+ianmagdaleno@users.noreply.github.com>
2020-05-31 18:35:39 -0300

    puzzle cristao

--- QuintoSemestre/Assets/Arido/Scripts/Hindu/HinduPuzzle.cs
+++ QuintoSemestre/Assets/Arido/Scripts/Hindu/HinduPuzzle.cs
@@ -3,53 +3,88 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements.Experimental;
+using UnityEngine.UI;
+//using UnityEngine.UIElements;
 
 public class HinduPuzzle : MonoBehaviour
 {
+    //public Canvas puzzle1;
     public GameObject plataformas_Obj;
+    public GameObject puzzle1UI;
     public String[] plataformas;
     public bool errou_plataforma = false;
     public String[] ordem_Correta_Plataformas = { "Plataforma1", "Plataforma2", "Plataforma3", "Plataforma4", "Plataforma5" };
     int i = 0;
     float tempo;
-    
+    public bool canvaPuzzle1 = false;
+
+
+
     void Start()
     {
-        plataformas = new string [5];  
+        plataformas = new string[5];
     }
 
-    
+
     void Update()
     {
-        print(tempo);
+      //  if (GetComponentInChildren<Interacao>().nomeObjeto == "RODASAMSARA")
+        {
+            canvaPuzzle1 = true;
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            canvaPuzzle1 = false;
+        }
+
+        if (canvaPuzzle1)
+        {
+            puzzle1UI.SetActive(true);
+        }
+        else if (!canvaPuzzle1) puzzle1UI.SetActive(false);
+
+        #region PUZZLE 2
+        //print(tempo);
 
         if (errou_plataforma == true && tempo <= 4f)
         {
             tempo += Time.deltaTime;
         }
-        else if(tempo > 4f)
+        else if (tempo > 4f)
         {
             errou_plataforma = false;
             tempo = 0;
         }
+        #endregion
     }
-    
 
+    //PUZZLE PARTE 2
     void OnCollisionEnter(Collision collision)
     {
-        if(i < plataformas.Length) { 
-            if(!errou_plataforma) plataformas[i] = collision.transform.name;
 
-            if (plataformas[i] != ordem_Correta_Plataformas[i]) {
+        if (i < plataformas.Length)
+        {
+            if (!errou_plataforma) plataformas[i] = collision.transform.name;
+
+            if (plataformas[i] != ordem_Correta_Plataformas[i])
+            {
 
                 errou_plataforma = true;
-                while (i>= 0){ 
-                    plataformas[i] = null; 
+                while (i >= 0)
+                {
+                    plataformas[i] = null;
                     i--;
                 }
-            } 
+            }
             i++;
         }
+    }
+
+    public void ImprimeNome()
+    {
+        print(gameObject.name);
+
     }
 
 
