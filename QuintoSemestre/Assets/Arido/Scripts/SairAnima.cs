7cfef296159e7508ae05e832c04e5c06574f57dd
Victor <61842979+Dirtcake@users.noreply.github.com>
2020-06-11 15:24:18 -0300

    Cristão arrumado

---
+++ QuintoSemestre/Assets/Arido/Scripts/SairAnima.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SairAnima : MonoBehaviour
+{
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
